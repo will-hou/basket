d41867f2cda4752434802d9b2f46e84d75b883eb
Gregory Shklovski <gshklovs@uci.edu>
2024-02-18 08:26:18 -0800

    better ui for checkout

--- frontend/src/views/CheckoutView.jsx
+++ frontend/src/views/CheckoutView.jsx
@@ -2,26 +2,31 @@ import CheckOutCard from "../components/CheckOutCard";
 
 export default function CheckoutView() {
   return (
-    <div>
-      <h1>Checkout</h1>
+    <div className="font-body">
+      <h1 className="text-center text-6xl font-display text-primary m-8">
+        Checkout
+      </h1>
       <CheckOutCard
         itemName="Carrot"
         quantity={1}
-        farmName="Farm"
+        farmName="Tanaka Farm"
         itemImage="https://via.placeholder.com/150"
         price={2.0}
         numBuyers={2}
         buyersThreshold={5}
       />
       <CheckOutCard
-        itemName="Carrot"
-        quantity={1}
-        farmName="Farm"
+        itemName="Tomato"
+        quantity={4}
+        farmName="Daisy Farm"
         itemImage="https://via.placeholder.com/150"
         price={2.0}
-        numBuyers={2}
+        numBuyers={7}
         buyersThreshold={5}
       ></CheckOutCard>
+      <button className="fixed bottom-8 left-[25%] w-[50%] rounded-2xl text-xl font-body bg-primary active:bg-primary/80 text-white py-4">
+        Checkout
+      </button>
     </div>
   );
 }
