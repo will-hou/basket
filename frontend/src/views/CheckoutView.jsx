e30667d9fe9ef5846b530424ac5d5b537c1eca8d
Gregory Shklovski <gshklovs@uci.edu>
2024-02-18 08:44:38 -0800

    greg improvements

--- frontend/src/views/CheckoutView.jsx
+++ frontend/src/views/CheckoutView.jsx
@@ -1,5 +1,7 @@
 import CheckOutCard from "../components/CheckOutCard";
 import { useNavigate } from "react-router-dom";
+import Honeycomb from "../assets/honeycomb.jpeg";
+import Tomato from "../assets/tomato.jpeg";
 
 export default function CheckoutView() {
   const navigate = useNavigate();
@@ -9,10 +11,10 @@ export default function CheckoutView() {
         Checkout
       </h1>
       <CheckOutCard
-        itemName="Carrot"
+        itemName="Honeycomb"
         quantity={1}
         farmName="Tanaka Farm"
-        itemImage="https://via.placeholder.com/150"
+        itemImage={Honeycomb}
         price={2.0}
         numBuyers={2}
         buyersThreshold={5}
@@ -21,16 +23,22 @@ export default function CheckoutView() {
         itemName="Tomato"
         quantity={4}
         farmName="Daisy Farm"
-        itemImage="https://via.placeholder.com/150"
+        itemImage={Tomato}
         price={2.0}
         numBuyers={7}
         buyersThreshold={5}
       ></CheckOutCard>
       <button
         onClick={() => navigate("/confirmation")}
-        className="fixed bottom-8 left-[25%] w-[50%] rounded-2xl text-xl font-body bg-primary active:bg-primary/80 text-white py-4"
+        className="fixed bottom-24 left-[25%] w-[50%] rounded-2xl text-xl font-body bg-primary active:bg-primary/80 text-white py-4"
       >
         Checkout
+      </button>
+      <button
+        onClick={() => navigate(-1)}
+        className="fixed bottom-8 left-[25%] w-[50%] rounded-2xl text-xl font-body text-tertiary active:text-secondary py-4"
+      >
+        &#x2190; Back
       </button>
     </div>
   );
