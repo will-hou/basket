0bb3fc0173477141d47ff43dbbe3cc96a9d9efe6
Gregory Shklovski <gshklovs@uci.edu>
2024-02-17 19:02:09 -0800

    Onboard UI rough draft

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
 import "./App.css";
-
+import basket_logo from "./assets/basket_transparent.svg";
+import vercel_logo from "./assets/vercel.png";
 function App() {
   return (
-    <div>
-      <h1 className="font-display text-6xl m-10 text-center ">Basket</h1>
-      <div className="text-center mx-10 p-2 border bg-white/70">
-        <div className="text-start my-4">For existing users:</div>
-        <button className="bg-primary text-white active:bg-primary/80 px-4 py-2 rounded">
+    <div className="h-screen overflow-y-hidden">
+      <img src={basket_logo} className="h-[10rem] mx-auto mt-8" />
+      <p className="text-center mt-2 mb-8 text-base font-body font-semibold text-black/40">
+        <img className="inline h-4 opacity-50" src={vercel_logo} /> for farmers
+      </p>
+      <div className="text-start text-2xl font-body mx-10 p-4 border bg-white/70 rounded-2xl drop-shadow-md">
+        <div className="text-start mb-4">Existing Users</div>
+        <button className="bg-primary font-body text-lg text-white active:bg-primary/80 px-4 py-2 rounded-2xl">
           Log In
         </button>
-        <div className="text-start my-4">For new users:</div>
-        <button className="bg-secondary text-white active:bg-secondary/80 px-4 py-2 rounded">
-          Sign Up for Farmers
-        </button>
-        <button className="bg-secondary text-white active:bg-secondary/80 px-4 py-2 rounded">
-          Sign Up for Buyers
-        </button>
+        <div className="text-start mt-8">Create an Account</div>
+        <div className="text-start mb-4 text-base text-black/30">
+          Select an account type
+        </div>
+        <div className="flex flex-row">
+          <button className="inline-block border text-lg flex-1 border-secondary font-body text-secondary active:bg-secondary/10 px-4 py-2 mr-1 rounded-2xl">
+            Farmer
+          </button>
+          <button className="inline-block border text-lg flex-1 border-secondary font-body text-secondary active:bg-secondary/10 px-4 py-2 ml-1 rounded-2xl">
+            Shopper
+          </button>
+        </div>
       </div>
     </div>
   );
