5aeb49665c70e77ba5517f7d92004d12dd81d6d2
William Hou <whou125@gmail.com>
2024-02-18 08:54:54 -0800

    Update tagline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,8 @@ function App() {
         Basket
       </h1>
       <p className="text-center mt-2 mb-8 text-base font-body font-semibold text-black/40">
-        <img className="inline h-4 opacity-50" src={vercel_logo} /> for farmers
+        {/* <img className="inline h-4 opacity-50" src={vercel_logo} /> for farmers */}
+        Fresh groceries, delivered together.
       </p>
       <div className="text-center text-2xl font-body mx-10 p-4 border bg-white/70 rounded-2xl drop-shadow-md">
         <Auth />
